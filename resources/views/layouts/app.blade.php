061f1032434b270fcc2b06b977ee35b182ff4602
Dario La Padula <dario86ostia@gmail.com>
2022-12-18 20:18:40 +0100

    Removed meta row

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.87.0">
     <title>Test Cloud Care</title>
 
     <!-- Bootstrap core CSS -->
